d3d64291bd2531f487c9cb2cad7c893339f3a68c
Yash Shukla <yshukla2775@gmail.com>
2026-05-16 00:50:04 +0530

    fixed typo error in schoolController

--- src/controllers/schoolControllers.controllers.js
+++ src/controllers/schoolControllers.controllers.js
@@ -1,5 +1,5 @@
 import db from "../db/index.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import validateSchool from "../validations/schoolValidation.js";
